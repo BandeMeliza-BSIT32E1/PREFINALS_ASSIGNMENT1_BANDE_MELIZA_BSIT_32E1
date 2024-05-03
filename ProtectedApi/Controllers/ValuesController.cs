b3c24dddd6dcc639ae1005a064df0c7d9f1a5d82
Bande_Meliza_BSIT32E1 <155070275+BandeMeliza-BSIT32E1@users.noreply.github.com>
2024-05-03 17:39:36 +0800

    Update ValuesController

--- ProtectedApi/Controllers/ValuesController.cs
+++ ProtectedApi/Controllers/ValuesController.cs
@@ -1,33 +1,32 @@
-﻿namespace ProtectedApi
-{
-    using Microsoft.AspNetCore.Mvc;
-    using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
+namespace ProtectedApi
+{
     [ApiController]
     [Route("[controller]")]
     public class ValueController : ControllerBase
     {
         private readonly string _owner = "Meliza Bande";
+        private readonly Random _random = new Random();
+        private readonly string[] _thingsAboutOwner = new[]
+        {
+            "I love reading Wattpad books and app.",
+            "I enjoy experimenting with new programming languages.",
+            "I am passionate about machine learning and artificial intelligence.",
+            "I like crocheting.",
+            "I love sleeping.",
+            "I love eating.",
+            "I play on my phone when I have free time.",
+            "I love iced coffee.",
+            "I love watching K-drama.",
+            "I enjoy cooking and trying out new recipes."
+        };
 
         [HttpGet("about/me")]
         public IActionResult AboutMe()
         {
-            var random = new Random();
-            var thingsAboutOwner = new[]
-            {
-                "I love reading wattpad books and app.",
-                "I enjoy experimenting with new programming languages.",
-                "I am passionate about machine learning and artificial intelligence.",
-                "I like crocheting.",
-                "I love sleeping.",
-                "I love eating.",
-                "I play in my phone when I have free time",
-                "I love iced coffee.",
-                "I love watching kdrama",
-                "I enjoy cooking and trying out new recipes."
-            };
-            var thing = thingsAboutOwner[random.Next(thingsAboutOwner.Length)];
-
+            var thing = _thingsAboutOwner[_random.Next(_thingsAboutOwner.Length)];
             return Ok(thing);
         }
 
